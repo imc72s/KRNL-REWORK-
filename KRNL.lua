54ed557cd51c0db3c927a80dae0f2a984c006de1
IMC <sonsri1758888@gmail.com>
2025-12-09 13:36:47 +0700

    Update KRNL.lua

--- KRNL.lua
+++ KRNL.lua
@@ -13,8 +13,8 @@ designed using localmaze gui creator
 local LMG2L = {};
 
 -- Players.4Q4II.PlayerGui.ScreenGui
-LMG2L["ScreenGui_1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
-LMG2L["ScreenGui_1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+LMG2L["ScreenGui_1"] = Instance.new("ScreenGui", game:CoreGui)
+LMG2L["ScreenGui_1"]["ZIndexBehavior"] = 999999999999999;
 
 
 -- Players.4Q4II.PlayerGui.ScreenGui.Frame
